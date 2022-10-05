7c670df07991675f90773e34bfb05e560d54d9cc
SesarLosano <cesar.lozano.argueso@gmail.com>
2022-10-05 21:40:10 +0200

    Modificado tester

--- src/tests/ResolverTest.java
+++ src/tests/ResolverTest.java
@@ -1,36 +1,25 @@
 package tests;
 
 import sudoku.*;
-import utils.Coordenada;
+import utils.*;
 
 public class ResolverTest {
 
 
-    static Sudoku sudoku1 = new Sudoku("files/In2.text");
-    static Sudoku sudoku2 = new Sudoku("files/In1.text");
+    static Sudoku sudoku1 = new Sudoku("files/In1.text");
+    static Sudoku sudoku2 = new Sudoku("files/In2.text");
 
     public static void resolverSudoku(Sudoku sudoku){
         Resolver resolver = new Resolver(sudoku);
-
-        resolver.resolver();
-
-        System.out.println("");
-
-        sudoku .imprimirSudoku();
-
-        /* Coordenada coordenada1 = new Coordenada(4,5);
-
-        System.out.println("\nComprobando coordenada: " + coordenada1.toString()); 
-
-        System.out.println("\n" + new NumerosCandidatos(sudoku1, coordenada1).toString());
-
-
-        System.out.println("\n" + new NumerosCandidatos(sudoku1, coordenada1).toString());
         
-        System.out.println(resolver.matrizCandidatos[coordenada1.getX()-1][coordenada1.getY()-1].size());
- */
-        System.out.println("\n");
         resolver.imprimirMatriz();
+        resolver.asignarNumero(1, new Coordenada(1, 1));
+        resolver.imprimirMatriz();
+        resolver.asignarNumero(5, new Coordenada(1, 2));
+        resolver.imprimirMatriz();
+        /* resolver.resolver();
+        
+        sudoku.imprimirSudoku(); */
     }
 
     public static void main(String[] args){
