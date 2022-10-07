096a440acc4dc1327fb9587fd96b9dec9ead0135
SesarLosano <cesar.lozano.argueso@gmail.com>
2022-10-07 10:53:11 +0200

    Modificado tester

--- src/tests/ResolverTest.java
+++ src/tests/ResolverTest.java
@@ -5,6 +5,16 @@ import utils.*;
 
 public class ResolverTest {
 
+    //Declaracion de coordenadas
+    static Coordenada coordenada1 = new Coordenada(1, 1);
+    static Coordenada coordenada2 = new Coordenada(2, 5);
+    static Coordenada coordenada3 = new Coordenada(3, 2);
+    static Coordenada coordenada4 = new Coordenada(3, 4);
+    static Coordenada coordenada5 = new Coordenada(3, 5);
+    static Coordenada coordenada6 = new Coordenada(4, 4);
+    static Coordenada coordenada7 = new Coordenada(6, 2);
+    static Coordenada coordenada9 = new Coordenada(7, 8);
+    static Coordenada coordenada10 = new Coordenada(9, 4);
 
     static Sudoku sudoku1 = new Sudoku("files/In1.text");
     static Sudoku sudoku2 = new Sudoku("files/In2.text");
