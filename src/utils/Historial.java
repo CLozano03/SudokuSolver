26016dedd50e85ea990495b1652ae85dccb087d0
SesarLosano <cesar.lozano.argueso@gmail.com>
2022-10-08 18:40:05 +0200

    First commit

---
+++ src/utils/Historial.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Esta clase se va a encargar de guardar en una LinkedList cada uno de los 
+ * diferentes sudokus despues de asignarNumero()
+ */
+
+public class Historial {
+    
+}
