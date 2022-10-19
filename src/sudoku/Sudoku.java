9d996fc4cb58919230a2d43673aff4a0603dd3ca
César <98660262+CLozano03@users.noreply.github.com>
2022-10-19 23:36:35 +0200

    parcheado el esCorrecto, cambio temporal en funcion hayNFila (#5)

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -5,29 +5,31 @@ import stdlib.*;
 
 /**
  * Implementación de {@code ISudoku}
+ * 
  * @author Cesar Lozano
  */
 public class Sudoku implements ISudoku {
 
-    //Matriz principal del sudoku
+    // Matriz principal del sudoku
     public int[][] matrizSudoku;
 
-    //Declaracion de constantes utiles.
+    // Declaracion de constantes utiles.
     public static final int[] NUMEROS_SUDOKU = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     public static final int TAMANO_SUDOKU = 9;
 
     /**
      * Array tridimensional. Cada elemento de la 3x3 contiene
-     *  un array con los 9 elementos del cuadrado asociado.
+     * un array con los 9 elementos del cuadrado asociado.
      */
     public int[][][] cuadrados = new int[3][3][9];
 
     /**
-     * Pone en {@value matrizSudoku} cada uno de los 
+     * Pone en {@value matrizSudoku} cada uno de los
      * numeros leidos en el archivo "filename"
      * 
-     * Por otro lado, pone cada uno de los numeros del sudoku 
+     * Por otro lado, pone cada uno de los numeros del sudoku
      * en la matriz tridimensional de nombre: cuadrados
+     * 
      * @param filename
      */
     public Sudoku(String filename) {
@@ -35,7 +37,7 @@ public class Sudoku implements ISudoku {
         In sc = new In(filename);
         matrizSudoku = new int[TAMANO_SUDOKU][TAMANO_SUDOKU];
 
-        //Matriz del sudoku
+        // Matriz del sudoku
         for (int i = 0; i < TAMANO_SUDOKU; i++) {
             for (int j = 0; j < TAMANO_SUDOKU; j++) {
                 matrizSudoku[i][j] = sc.readInt();
@@ -43,14 +45,30 @@ public class Sudoku implements ISudoku {
         }
         sc.close();
 
-        //Creacion de la matriz de cuadrdados
+        // Creacion de la matriz de cuadrdados
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 for (int k = 0; k < 9; k++) {
-                    cuadrados[i][j][k] = matrizSudoku[i * 3 + (int) k / 3][j * 3 + k % 3];
+                    cuadrados[i][j][k] = matrizSudoku[i * 3 + (int) k / 3][j * 3 + k % 3]; // Los numeros
                     /*
-                        * COMENTAR ESTO
-                        */
+                     * i*3 y j*3 se refieren al cuadrado 3x3 del sudoku al que apunta.
+                     * - con k/3 y k%3 recorremos esa matriz 3x3 con solamente un iterador
+                     * - k/3 --> filas
+                     * - k%3 --> columnas
+                     * EJEMPLO => Si un cuadrado es esto:
+                     * 5 0 7
+                     * 6 2 0
+                     * 1 0 0
+                     * Los numeros se meten de esta forma: 5 0 7 6 2 0 1 0 0
+                     * 
+                     * 
+                     * PARA ACCEDER A UN NUMERO A TRAVES DE UNA COORDENADA:
+                     * 
+                     * FILAS COLUMNAS
+                     * 3 *((coordenada.getX() -1) % 3) + (coordenada.getX() -1) % 3
+                     * 
+                     * 
+                     */
                 }
             }
         }
@@ -89,7 +107,7 @@ public class Sudoku implements ISudoku {
     public int getN(Coordenada coordenada) {
         return matrizSudoku[coordenada.getX() - 1][coordenada.getY() - 1];
     }
-    
+
     @Override
     public void setN(Coordenada coordenada, int n) {
         matrizSudoku[coordenada.getX() - 1][coordenada.getY() - 1] = n;
@@ -97,30 +115,32 @@ public class Sudoku implements ISudoku {
 
     @Override
     public boolean hayN(int numero, Coordenada coordenada) {
-        return hayNFila(numero, coordenada.getX(), true) || hayNFila(numero, coordenada.getY(), false)
-                || hayNCuadrado(numero, coordenada);
+        return /*
+                * hayNFila(numero, coordenada.getX(), true) || hayNFila(numero,
+                * coordenada.getY(), false)
+                * ||
+                */hayNCuadrado(numero, coordenada);
     }
 
     @Override
-    public boolean hayNFila(int numero, int numeroFila, boolean filaColumna) {
+    public boolean hayNFila(int numero, Coordenada coordenada) {
         boolean hayN = false;
-        if (filaColumna) {
             for (int i = 0; i < 9 && !hayN; i++) {
-                hayN = (matrizSudoku[numeroFila - 1][i] == numero);
+                hayN = ((coordenada.getY() == i + 1) &&(matrizSudoku[coordenada.getX() - 1][i] == numero)) || 
+                        ((coordenada.getY() == i + 1) && matrizSudoku[i][coordenada.getY() - 1] == numero);
+                //Quiero que la i no sea 
             }
+
+            
             return hayN;
-        } else {
-            for (int i = 0; i < 9 && !hayN; i++) {
-                hayN = (matrizSudoku[i][numeroFila - 1] == numero);
-            }
-            return hayN;
-        }
     }
 
-    public boolean hayNCuadrado(int numero, Coordenada coordenada) {
+    public boolean hayNCuadrado(int numero, Coordenada coordenada) { // 1 1 | 2 3
         boolean hayN = false;
+        int casillaNoComprobar = 3 * ((coordenada.getX() - 1) % 3) + ((coordenada.getY() - 1) % 3); // Consultar constructor
         for (int i = 0; i < 9 && !hayN; i++) {
-            hayN = cuadrados[(int) ((coordenada.getX() - 1) / 3)][(int) ((coordenada.getY() - 1) / 3)][i] == numero;
+            if (i != casillaNoComprobar)
+                hayN = cuadrados[(int) ((coordenada.getX() - 1) / 3)][(int) ((coordenada.getY() - 1) / 3)][i] == numero;
         }
         return hayN;
     }
@@ -137,23 +157,22 @@ public class Sudoku implements ISudoku {
                 completo = !estaVacia(coordenada);
             }
         }
-
         return completo;
     }
 
-
-    //LA implementacion de esta funcion falla porq el metodo hayN comprueba la misma coordenada a la que apunta, cuando no deberia
-    //Cambiar hayN y/o resto de llamadas
+    // LA implementacion de esta funcion falla porq el metodo hayN comprueba la
+    // misma coordenada a la que apunta, cuando no deberia
+    // Cambiar hayN y/o resto de llamadas
     @Override
-    public boolean esCorrecto(){
+    public boolean esCorrecto() {
         boolean esCorrecto = true;
         Coordenada coordenadaAux = new Coordenada(0, 0);
-        
-        for(int i = 1; i <= Sudoku.TAMANO_SUDOKU && esCorrecto; i++){
-            for(int j = 1; j <= Sudoku.TAMANO_SUDOKU && esCorrecto; j++){
-                coordenadaAux.setXY(i,j);
-                
-                if(!estaVacia(coordenadaAux)){
+
+        for (int i = 1; i <= Sudoku.TAMANO_SUDOKU && esCorrecto; i++) {
+            for (int j = 1; j <= Sudoku.TAMANO_SUDOKU && esCorrecto; j++) {
+                coordenadaAux.setXY(i, j);
+
+                if (!estaVacia(coordenadaAux)) {
                     esCorrecto = !hayN(getN(coordenadaAux), coordenadaAux);
                 }
             }
@@ -162,7 +181,7 @@ public class Sudoku implements ISudoku {
     }
 
     @Override
-    public boolean sudokuResuelto(){
+    public boolean sudokuResuelto() {
         return estaCompleto() && esCorrecto();
     }
 
@@ -175,10 +194,10 @@ public class Sudoku implements ISudoku {
         }
         Sudoku s = (Sudoku) o;
         boolean iguales = true;
-        
-        //Recorro las matrices
-        for(int i = 0; i < Sudoku.TAMANO_SUDOKU && iguales; i++) {
-            for(int j = 0; j < Sudoku.TAMANO_SUDOKU && iguales; j++) {
+
+        // Recorro las matrices
+        for (int i = 0; i < Sudoku.TAMANO_SUDOKU && iguales; i++) {
+            for (int j = 0; j < Sudoku.TAMANO_SUDOKU && iguales; j++) {
                 iguales = matrizSudoku[i][j] == s.matrizSudoku[i][j];
             }
         }
